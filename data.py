1f7c0a53f0aa06ee1a9ccccd6343d348455fa130
valearz <51324698+valearz@users.noreply.github.com>
2023-05-09 12:21:17 -0600

    carpetas

---
+++ data.py
@@ -0,0 +1,7 @@
+
+# Capital inicial: $100,000 
+# Elegir un par de divisas (ej. USDJPN , EURUSD ... ) 
+# Riesgo máximo por operación (pérdida máximo): $1,000 usd
+# Divide los datos de la siguiente manera:  
+# Periodo de entrenamiento: 01/ene/2020 - 01/ene/2021
+# Periodo de prueba: 01/Feb/2021 - 01/Feb/2022
